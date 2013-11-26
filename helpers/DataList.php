370e4c3a43ec444f9d769cb7149d965556007155
Semyon <webmaster@rere-design.ru>
2013-11-26 11:49:32 +0600

    update parent select in page

--- helpers/DataList.php
+++ helpers/DataList.php
@@ -19,13 +19,25 @@ class DataList
         return self::getter(__METHOD__ . $condition, 'Module', 'id', 'name', $condition);
     }
 
+    static function parentsTree($module_id)
+    {
+        $result = $parents = array();
+        $data = Page::model()->findAll(array('with' => array('rName'), 'order' => 't.lft', 'condition' => 't.module_id=:module_id', 'params' => compact('module_id')));
+        foreach ($data as $row) {
+            $parents[$row->id] = $row->name;
+            if (empty($parents[$row->parent_id])) $result[$row->id] = $row->name;
+            else $result[$parents[$row->parent_id]][$row->id] = $row->name;
+        }
+        return $result;
+    }
+
     static function getter($id, $object, $key, $value, $condition)
     {
         if (empty($remember[$id])) {
-            $user = RActiveRecord::model($object);
-            $sql = 'SELECT ' . $key . ', ' . $value . ' FROM `' . $user->tableName() . '`';
+            $model = RActiveRecord::model($object);
+            $sql = 'SELECT ' . $key . ', ' . $value . ' FROM `' . $model->tableName() . '`';
             if ($condition) $sql .= ' WHERE ' . $condition;
-            $remember[$id] = CHtml::listData($user->findAllBySql($sql), $key, $value);
+            $remember[$id] = CHtml::listData($model->findAllBySql($sql), $key, $value);
         }
         return $remember[$id];
     }
