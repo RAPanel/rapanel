b89ecb4fb024f9516a5d2ea32c389ba3d54037b4
Anatoly Rugalev <anatoly.rugalev@gmail.com>
2014-07-23 15:53:34 +0600

    Sidebar position remember

--- assets/application.js
+++ assets/application.js
@@ -24,13 +24,14 @@ $(function () {
 		if ($(this).hasClass('active')) {
 			left = 0;
 			$('.resize').draggable({containment: [200, 0, 500, 0]});
+			sessionStorage.setItem('asideClosed', 0);
 		}
 		else {
 			left = -block.outerWidth() + 5;
 			$('.resize').draggable({containment: [0, 0, 0, 0]});
+			sessionStorage.setItem('asideClosed', 1);
 		}
 		block.animate({'margin-left': left}, 200);
-		sessionStorage.setItem('asideClosed', true);
 		$(this).toggleClass('active');
 		$('section.main').animate({'margin-left': left ? 15 : 0}, 200, function () {
 			$(window).trigger('resize');
@@ -49,7 +50,7 @@ $(function () {
 	if (sessionStorage.getItem("menuWidth")) {
 		block.width(sessionStorage.getItem("menuWidth"));
 	}
-	if (sessionStorage.getItem('asideClosed')) {
+	if (sessionStorage.getItem('asideClosed') == 1) {
 		$('.hide', block).trigger('click');
 	}
 
